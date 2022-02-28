3edd7e042382632bf75ada5c826c962e1b737823
ievaluizedrava <98715615+ievaluizedrava@users.noreply.github.com>
2022-02-28 15:19:33 +0200

    laukums

--- script.js
+++ script.js
@@ -20,14 +20,58 @@ function perimetrs(m1, m2, m3) {
     const p = m1 + m2 + m3;
     console.log(p);
     return (p);
-} function pusper(m1, m2, m3) {
+}
+function pusper(m1, m2, m3) {
     const pusper = perimetrs(m1+m2+m3)/2;
     console.log(pusper);
     return (pusper);
 }
 function laukums(m1,m2,m3){
     const pusper=perimetrs(m1,m2,m3)/2;
-let tlaukums=Math.sqrt(pusper*((pusper-m1)*(pusper-m2)*(puspe-m3)));
+let tlaukums=Math.sqrt(pusper*((pusper-m1)*(pusper-m2)*(pusper-m3)));
 console.log(tlaukums);
 return (tlaukums);
+}
+function irTrijsturis(m1,m2,m3){
+    if (m1<m2+m3 && m2<m1+m3 && m3<m1+m2){
+        console.log(true);
+        return true;
+    } else{
+        console.log(irTrijsturis);
+        return false;
+    }
+}
+function rezultats() {
+ const m=nolasa();
+m1+m.m1;m2=m.m2;m3=m.m3;
+console.log(m1,m2,m3);
+if (!nolasa()){
+t="Nekorekti ievaddati, trijstura malu vertibām ir jābūt lielākam par nulli";
+} else {
+    if (irTrijsturis(M1,M2,M3)==false){
+        t="Trijsturis neeksistē. jo jebkuru 2 malu garumu summai ir jābūt lielākai par trešo malu!";
+    }else{
+        t="Trijstūris ar malu garumiem"+m1+","+m2+"un"+m3+"eksistē.";
+        if (m1==m2 && m2==m3){
+            t+="Tas ir vienādmalu trijstūris";
+        } else{
+            if(m1==m2 || m2==m3 || m1==m3){
+
+            }
+        }
+    const p=perimets(m1.m2,m3);
+    const s=Math.round(laukumms(m1,m2,m3)*100)/100;
+t+="Perimetrs ir"+p+"un laukums ir"+s+".";
+}
+}
+console.log(t);
+return t;
+}
+function izvadaTekstu(){
+    const teksts=rezultats();
+    console.log(teksts);
+    const sakne=document.getElementById("izvade");
+    const raksti=document.createElement("p");
+    sakne.innerHTML=teksts;
+    sakne.appendChild(raksti);
 }
